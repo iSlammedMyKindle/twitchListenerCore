069a3c66e862dbde8f8bffcf51c6ccb86f2ff353
iSlammedMyKindle <108752129+iSlammedMyKindle@users.noreply.github.com>
2024-02-14 23:00:51 -0500

    Added oauth to help restart the server when it breaks updated to the latest version of twurple

--- index.mjs
+++ index.mjs
@@ -9,12 +9,16 @@ import { ChatClient } from "@twurple/chat";
 import { EventSubWsListener } from "@twurple/eventsub-ws";
 import { evtSubList, twitchMsg } from "./listeners.mjs";
 import { readFile, writeFile, access } from "fs/promises";
-import { twitchConfig as config } from "./configExport.mjs";
+import { twitchConfig as config, twitchConfig } from "./configExport.mjs";
 import twitchEmitter from "./twitchEmitter.mjs";
+import { createServer } from "http";
+import otp from "otp";
 import "./wsServer.mjs";
 
 async function fetchToken(config = {}, tokensDir = './tokens.json'){
     try{
+        if(config.newToken) throw new Error('Getting a new token on purpose');
+
         await access(tokensDir);
 
         console.log('Found '+tokensDir+'! Using that to login');
@@ -31,49 +35,82 @@ async function fetchToken(config = {}, tokensDir = './tokens.json'){
 
 // Authenticate and login to twitch
 // Figure out if we have a token saved - if not, create a new one by authenticating twitch
-var tokenData = await fetchToken(config);
+var tokenData;
+var evtSub;
+var chatClient;
 
-// https://twurple.js.org/reference/pubsub/classes/BasicPubSubClient.html#listen
-// For each thing you're listening to, you'll need to place your token inside the .listen function
-const authProvider = new RefreshingAuthProvider({
-    clientId: config.twitch_auth.client_id,
-    clientSecret: config.twitch_auth.client_secret,
-});
+async function init(newToken = false){
+    tokenData = await fetchToken({...config, newToken});
+    // Stop the original eventSub listener
+    evtSub?.stop();
+    chatClient?.quit();
 
-authProvider.onRefresh(async (userId, newTokenData)=>{
-    tokenData = newTokenData;
-    return await writeFile('./tokens.json', JSON.stringify(newTokenData), 'utf-8');
-});
+    // https://twurple.js.org/reference/pubsub/classes/BasicPubSubClient.html#listen
+    // For each thing you're listening to, you'll need to place your token inside the .listen function
+    const authProvider = new RefreshingAuthProvider({
+        clientId: config.twitch_auth.client_id,
+        clientSecret: config.twitch_auth.client_secret,
+    });
+    
+    authProvider.onRefresh(async function(userId, newTokenData){
+        tokenData = newTokenData;
+        return await writeFile('./tokens.json', JSON.stringify(newTokenData), 'utf-8');
+    });
+    
+    //One refreshing auth provider should give us the client for everything
+    
+    //Api client - used to grab specific things not directly related to the current action (specific user ID is required to get things going here in order to detect channel point redeems)
+    const apiClient = new ApiClient({ authProvider });
+    
+    authProvider.addUser((await (getTokenInfo(tokenData.accessToken, config.twitch_auth.clientId))).userId, tokenData, ['chat', 'channel']);
+    
+    //Messages client - for every message in chat, ping it back to the events
+    chatClient = new ChatClient({ authProvider, channels:config.twitch_auth.channels });
+    chatClient.onMessage(twitchMsg);
+    chatClient.connect();
+    
+    // EventSub
+    // Channel points
+    const targetChannel = await apiClient.users.getUserByName(config.twitch_auth.channels[0]);
+    
+    evtSub = new EventSubWsListener({ apiClient });
+    
+    // Load the listeners for EventSub:
+    for(var key in evtSubList)
+        evtSub["on"+(evtSubList[key].apiName)](targetChannel.id, ...(evtSubList[key].modParam ? [targetChannel.id, evtSubList[key].func] : [evtSubList[key].func]));
+    
+    evtSub.start();
+    console.log("Everything connected!");
+}
 
-//One refreshing auth provider should give us the client for everything
-
-//Api client - used to grab specific things not directly related to the current action (specific user ID is required to get things going here in order to detect channel point redeems)
-const apiClient = new ApiClient({ authProvider });
-
-authProvider.addUser((await (getTokenInfo(tokenData.accessToken, config.twitch_auth.clientId))).userId, tokenData,['chat', 'channel']);
-
-//Messages client - for every message in chat, ping it back to the events
-const chatClient = new ChatClient({ authProvider, channels:config.twitch_auth.channels });
-chatClient.onMessage(twitchMsg);
-chatClient.connect();
-
+// twitchEmitter is custom so it can be separate from the twurple itself.
 twitchEmitter.on('clientmsg', (channel, text, replyTo)=>{
     chatClient.say(channel, text, replyTo).then(
         e=>console.log('client:', channel, text, replyTo), 
         e=>console.error('Failed to send client message -_-', e)
     );
-})
+});
 
-// EventSub
-// Channel points
-const targetChannel = await apiClient.users.getUserByName(config.twitch_auth.channels[0]);
+init();
 
-const evtSub = new EventSubWsListener({ apiClient });
+// This server here is made to reach out in case the core decides to die. One request and it will re-fetch the token upon browser prompt
+// setup otp authentication via get parameters
+const otpInstance = otp.parse(twitchConfig.otp);
+const refreshServer = createServer((req, res)=>{
+    console.log(new URLSearchParams(req.search).get('otp'), otpInstance.totp());
+    if(new URLSearchParams(req.url.split('?')[1]).get('otp') != otpInstance.totp()){
+        res.statusCode = 404;
+        res.end();
+        return;
+    }
 
-// Load the listeners for EventSub:
-for(var key in evtSubList)
-    evtSub["on"+(evtSubList[key].apiName)](targetChannel.id, ...(evtSubList[key].modParam ? [targetChannel.id, evtSubList[key].func] : [evtSubList[key].func]));
+    // If the code is correct, initiate a server reboot, but don't disconnect existing clients
+    console.log('Restart requested, re-running init()');
+    res.statusCode = 200;
+    res.write('restarting!');
+    res.end();
 
-evtSub.start();
+    init(true);
+});
 
-console.log("Everything connected!");
+refreshServer.listen('9100');
--- readme.md
+++ readme.md
@@ -16,6 +16,22 @@ To start listening to things through the listener core, your connected applicati
 
 If you want to add more listeners, they can be created in `./configs/listeners.json`. You can discover more through the [twurple documentation](https://twurple.js.org/reference/eventsub-ws/classes/EventSubWsListener.html). Right now the listener core only goes as deep as primitives, it will likely parse objects into JSON if you list those variables, I haven't tested this though.
 
+### OTP
+
+If twitchListenerCore dies (it no longer listens to twitch), you can reboot it using an OTP 6-digit key. This allows all clients to remain connected while re-connecting to twitch. To make a 6-digit key, run `node` in the console and execute the following:
+
+```js
+require('otp').parse();
+```
+
+it will give you a `secret`, e.g `VD7[...]G3SVI=`. Make something like the following in a QR-code to get a 6-digit code in something like authy: `oauth://totp/twitchListenerCore?secret=VD7[...]G3SVI=`
+
+Finally, when the server is running, make a call to the server on port `9100`
+
+`http://localhost:9100?otp=123456`
+
+Where `123456` is the 6-digit code that the authenticator application gives you.
+
 ## Mocking
 
 You can use the mock server (`./tests/mockServer.mjs`) to test the listeners without connecting to twitch. The exported listeners are in their rawest form, so executing them requires arguments on your end.
